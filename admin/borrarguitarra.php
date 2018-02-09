160565df1b3aceb5902a71012ded0dc21bc5dd32
David Leza <leza1313@gmail.com>
2018-02-09 13:34:33 +0100

    Backend,segunda pruebas añadido fichero borrarguitarra.

--- admin/borrarguitarra.php
+++ admin/borrarguitarra.php
@@ -2,9 +2,9 @@
     include("config.php");
     session_start();
     if($_SERVER["REQUEST_METHOD"] == "GET") {
-        $mynombre = mysqli_real_escape_string($db,$_GET['nombre']);
+        $mynombre = mysqli_real_escape_string($db,$_GET['varname']);
         
-        $sql = "DELETE FROM guitarras WHERE varname=$mynombre;";
+        $sql = "DELETE FROM guitarras WHERE nombre=$mynombre;";
         echo $sql;
         $result = mysqli_query($db,$sql);
         
