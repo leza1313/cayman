8f9b66cfe058d4ac9d22f3aae07ece6bc5a86a8b
David Leza <leza1313@gmail.com>
2018-02-12 20:43:28 +0100

    Añadido formulario y funcion que añade usuarios

---
+++ admin/anadirusuario.php
@@ -0,0 +1,30 @@
+<?php
+include('session.php');
+include('config.php');
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+    
+      $sql = "SELECT id FROM usuarios WHERE id = '$myusername'";
+
+        $result = mysqli_query($db,$sql);
+
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    
+        $count = mysqli_num_rows($result);
+    
+        if($count==1){
+            echo "Ya existe un usuario con ese nombre";
+        }else{
+            $hash = password_hash($mypassword,PASSWORD_DEFAULT);
+            $sql = "INSERT INTO usuarios (id, pass) VALUES ('$mynombre', '$hash');";
+            $result = mysqli_query($db,$sql);
+            echo "Usuario creado correctamente.";
+            echo "<a href='../index.php'>Volver al Inicio</a>";
+        }
+}
+$db->close();
+?>
--- admin/crearcuenta.php
+++ admin/crearcuenta.php
@@ -1,4 +1,46 @@
 <?php
     include('session.php');
-
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="../css/style.css">
+        
+
+        <title>Crear Cuenta</title>
+    </head>
+    <body id="cover">
+        <br>
+        <a href="../index.php" class="btn btn-info">INICIO</a>
+        <br>
+        <br>
+        <div align = "center" >
+         <div style = "width:300px; border: solid 1px #333333; background-color: #666 " align = "left">
+            <div style = "background-color:#444; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "anadirusuario.php" method = "post" bg: #fff>
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>	
+            </div>
+         </div>
+      </div>
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="../js/jquery-3.2.1.min.js"></script>
+        <script src=".../js/bootstrap.min.js"></script>
+    </body>
+</html>
