ee1ec28271ded025a02a12379be957403900d6ec
David Leza <leza1313@gmail.com>
2018-02-12 16:19:39 +0100

    Añadido creacion dinamica de las cartas de muestra de los bajos desde la bbdd

--- bajos.php
+++ bajos.php
@@ -14,19 +14,51 @@
                        <div class="card-group">
                          
                          <!-- Hacer esto dinamico on php para cada fila de la tabla guitarras-->
-                          <!-- Primera carta-->
+                          <!-- Primera carta
                            <div id='bloque-carta' class="card rounded">
                               <?php
                                 //Coger de la base de datos informacion de las guitarras
-                                $var_value = 'BAJOS';
+                                //$var_value = 'mississipi';
                                ?>
                                <div class="card-block">
-                                   <h4 id="titulo-carta" class="card-title"><?php echo $var_value;?></h4>
+                                   <h4 id="titulo-carta" class="card-title"><?php //echo $var_value;?></h4>
                                    
-                                   <a href="infobajo.php?varname=<?php echo $var_value ?>"><img src="/img/teleca.jpeg" alt="teleca"></a>
+                                   <a href="infoguitar.php?varname=<?php //echo $var_value ?>"><img src="/img/teleca.jpeg" alt="teleca"></a>
                                    
                                </div>
-                           </div>
+                           </div>-->
+                           
+                           
+                           <?php
+
+
+                                include('admin/config.php');
+                                $sql = "SELECT nombre,foto FROM bajos";
+                                $result = mysqli_query($db,$sql);
+                                $num_cards=0;
+                                $num_rows=0;
+                                if ($result->num_rows > 0) {
+                                    // output data of each row
+                                    while($row = $result->fetch_assoc()) {
+                                        echo "<div id='bloque-carta' class='card rounded col-sm-4'>";
+                                        echo "<div class='card-block'>";
+                                        echo "<h4 id='titulo-carta' class='card-title'>" . $row['nombre'] . "</h4>";
+                                        echo "<a href='infobajo.php?varname=" . $row['nombre'] . "'><img src='" . $row['foto'] . "' alt='" . $row['nombre'] . "'></a>";
+                                        echo "</div>";
+                                        echo "</div>";
+                                        $num_cards=$num_cards+1;
+                                        if ($num_cards>2){
+                                            echo "</div></div></div>";
+                                            echo "<br>";
+                                            echo "<div class='row'><div class='col-sm-12'><div class='card-group'>";
+                                            $num_cards=0;
+                                            $num_rows=$num_rows+1;
+                                        }
+                                    }
+                                } else {
+                                    echo "<h2>0 results</h2>";
+                                }
+                           ?>
                            
                        </div>
                    </div>
