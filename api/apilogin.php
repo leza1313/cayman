c6dd78ff764e955d40ad8182a59baf5f02e50fcc
David Leza <leza1313@gmail.com>
2018-02-15 19:33:35 +0100

    Prueba de apilogin.php para Basic Auth de la Restful API

--- api/apilogin.php
+++ api/apilogin.php
@@ -1,20 +1,13 @@
 <?php
-    include('../admin/config.php');
+    //include('../admin/config.php');
     //$_SERVER=null;
     if (!isset($_SERVER['PHP_AUTH_USER'])) {
         header('WWW-Authenticate: Basic realm="leza1313.hopto.org"');
         header('HTTP/1.0 401 Unauthorized');
         exit;
     }else{
-        //echo $_SERVER['PHP_AUTH_USER'];
-        //echo $_SERVER['PHP_AUTH_PW'];
-        //$username = base64_decode($_SERVER['PHP_AUTH_USER']);
-        //$password = base64_decode($_SERVER['PHP_AUTH_PW']);
-        //echo $username;
-        //echo $password;
         
-        
-        $myusername = mysqli_real_escape_string($db,$_SERVER['PHP_AUTH_USER']);
+        /*$myusername = mysqli_real_escape_string($db,$_SERVER['PHP_AUTH_USER']);
         $mypassword = mysqli_real_escape_string($db,$_SERVER['PHP_AUTH_PW']);
 
         $sql = "SELECT id,pass FROM usuarios WHERE id = '$myusername'";
@@ -28,7 +21,16 @@
         } else {
             echo 'La contraseña no es válida.';
             header('HTTP/1.0 401 Unauthorized');
-        exit;
+            exit;
+        }*/
+        if($_SERVER['PHP_AUTH_USER']=='david' && $_SERVER['PHP_AUTH_PW']=='1234'){
+            //echo "Contraseña valida!";
+        }else{
+            echo 'La contraseña no es válida.';
+            header('HTTP/1.0 401 Unauthorized');
+            $realm = mt_rand( 1, 1000000000 );
+            header( 'WWW-Authenticate: Basic realm='.$realm );
+            exit;
         }
     }
 ?>
---
+++ api/prueba.php
@@ -0,0 +1,4 @@
+<?php
+    require('apilogin.php');
+    echo "A";
+?>
