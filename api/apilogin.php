3d88e4e04617e25706bfe7800817142c48e62662
David Leza <leza1313@gmail.com>
2018-02-15 13:42:32 +0100

    Prueba de Basi Auth en apilogin

--- api/apilogin.php
+++ api/apilogin.php
@@ -1,14 +1,17 @@
+<?php
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="leza1313.hopto.org"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Texto a enviar si el usuario pulsa el botón Cancelar';
+    exit;
+} else {
+    echo "<p>Hola {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>Introdujo {$_SERVER['PHP_AUTH_PW']} como su contraseña.</p>";
+}
+?>
+
 <form method="post" action="">
 <input name="username" value=""/>
 <input name="password" value=""/>
 <input type="submit" value="ok">
 </form>
-
-<?php
-if ($_POST['username']=='admin' && $_POST['password']=='admin'){
-    session_start();
-    $_SESSION['login_user']='admin';
-}else{
-    echo 'ACCESO DENEGADO';
-}
-?>
